a6b8522dfb30aba299dc80b09899d9b66e580ec0
FourthSpade <prw00005@mix.wvu.edu>
2021-09-05 22:05:47 -0400

    Lab 2 completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 import { TopBarComponent } from './header/top-bar.component';
+import { SidebarComponent } from './sidebar/sidedar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    FooterComponent,
+    SidebarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'body', 
+    templateUrl:'body.component.html'
+})
+
+export class BodyComponent {
+
+}
